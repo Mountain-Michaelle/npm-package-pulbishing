a373943ee5aaba9ad88cdf435fa6e95201a5de52
unknown <michaelchinemelu24@gmail.com>
2026-03-16 09:29:18 -0800

    my first npm package commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function multiplication(a, b){
+    return a * b
+}
+
+
+function name(name) {
+    return "Welcome " + name 
+}
+module.exports = {multiplication, name};
---
+++ test.js
@@ -0,0 +1,8 @@
+
+const {multiplication, name} = require("./index.js")
+
+
+console.log(multiplication(10, 4))
+
+
+console.log('\n' + name("Amaka"));
